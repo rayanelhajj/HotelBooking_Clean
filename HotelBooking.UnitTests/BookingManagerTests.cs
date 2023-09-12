381999081f72fd0fbd37873c9bb85bddf1a9f965
Faust <faustis1337@gmail.com>
2023-09-12 11:26:35 +0200

    tests

--- HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking.UnitTests/BookingManagerTests.cs
@@ -9,7 +9,8 @@ namespace HotelBooking.UnitTests
     {
         private IBookingManager bookingManager;
 
-        public BookingManagerTests(){
+        public BookingManagerTests()
+        {
             DateTime start = DateTime.Today.AddDays(10);
             DateTime end = DateTime.Today.AddDays(20);
             IRepository<Booking> bookingRepository = new FakeBookingRepository(start, end);
@@ -37,6 +38,67 @@ namespace HotelBooking.UnitTests
             DateTime date = DateTime.Today.AddDays(1);
             // Act
             int roomId = bookingManager.FindAvailableRoom(date, date);
+            // Assert
+            Assert.NotEqual(-1, roomId);
+        }
+
+        [Fact]
+        public void FindAvailableRoom_StartDateInThePast()
+        {
+            // Arrange
+            DateTime startDate = DateTime.Today.AddDays(-5);
+            DateTime finishDate = startDate.AddDays(3);
+            // Act
+            Action act = () => bookingManager.FindAvailableRoom(startDate, finishDate);
+            // Assert
+            Assert.Throws<ArgumentException>(act);
+        }
+
+        [Fact]
+        public void FindAvailableRoom_OverlapsBookingButNotAvailableDays()
+        {
+            // Arrange
+            DateTime startDate = DateTime.Today.AddDays(6);
+            DateTime finishDate = startDate.AddDays(10);
+            // Act
+            int roomId = bookingManager.FindAvailableRoom(startDate, finishDate);
+            // Assert
+            Assert.Equal(-1, roomId);
+        }
+
+        [Fact]
+        public void FindAvailableRoom_OverlapsBookingPartiallyInclAvailableDays()
+        {
+            // Arrange
+            DateTime startDate = DateTime.Today.AddDays(1);
+            DateTime finishDate = startDate.AddDays(10);
+            // Act
+            int roomId = bookingManager.FindAvailableRoom(startDate, finishDate);
+            // Assert
+            Assert.Equal(-1, roomId);
+        }
+
+        [Fact]
+        public void FindAvailableRoom_OverlapsExistingBookingFullyInclAvailableDays()
+        {
+            // Arrange
+            DateTime startDate = DateTime.Today.AddDays(1);
+            DateTime finishDate = startDate.AddDays(35);
+            // Act
+            int roomId = bookingManager.FindAvailableRoom(startDate, finishDate);
+            // Assert
+            Assert.Equal(-1, roomId);
+        }
+
+
+        [Fact]
+        public void FindAvailableRoom_AfterBooking()
+        {
+            // Arrange
+            DateTime startDate = DateTime.Today.AddDays(30);
+            DateTime finishDate = startDate.AddDays(5);
+            // Act
+            int roomId = bookingManager.FindAvailableRoom(startDate, finishDate);
             // Assert
             Assert.NotEqual(-1, roomId);
         }
